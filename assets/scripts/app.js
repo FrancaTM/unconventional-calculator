e1469bdf6ac205c004543f679129b4db20ece2c5
FrancaTM <francatm@outlook.com>
2019-11-03 00:31:36 -0300

    create log entry object

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -18,9 +18,14 @@ function add() {
   const initialResult = currentResult;
   currentResult += enteredNumber;
   createAndWriteOutput("+", initialResult, enteredNumber);
-  logEntries.push(enteredNumber);
+  const logEntry = {
+    operation: "ADD",
+    prevResult: initialResult,
+    number: enteredNumber,
+    result: currentResult
+  };
+  logEntries.push(logEntry);
   console.log(logEntries);
-  console.log(logEntries[0]);
 }
 
 function subtract() {
